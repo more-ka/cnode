ed7850aedbc48cee5e9919b7699289788fd4fab0
more-ka <1640627628@qq.com>
2019-08-04 11:40:31 +0800

    Header组件完成

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+  .header{
+    height: 40px;
+    background: #444;
+    padding: 5px 80px;
+  }
+  img{
+    max-width: 120px;
+    margin: 4px 0;
+  }
+  ul{
+    list-style: none;
+    float: right;
+    text-align: center;
+  }
+  li{
+    display: inline-block;
+    padding: 10px 15px;
+  }
+  a{
+    text-decoration: none;
+    color: #ccc;
+  }
+</style>
